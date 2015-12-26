a0620efba2bb0ed564a497e0f02195a48928ee6f
Tal Almagor <tal.almagor@ironsrc.com>
2015-12-26 12:05:21 +0200

    mysql-migrate command line

--- bin/node-mysql-migrate.js
+++ bin/node-mysql-migrate.js
@@ -1,1 +1,96 @@
-// ToDo
+#!/usr/bin/env node
+
+var _ = require('lodash');
+var pkginfo = require('../package.json');
+var program = require('commander');
+var Table = require('cli-table2');
+var Config = require('../lib/config');
+var SchemaManager = require('../lib/schema');
+var exitCode = 0;
+
+/**
+ * Short circuit for the command line options.
+ * @param operation: String, operation name (e.g "migrate").
+ * @param arguments: Array, list of arguments for the operation.
+ * @param callback: Optional, callback function that will receive the output of the operation.
+ */
+function callOperationByName(operation, arguments, callback) {
+    callback = callback ? callback : _.noop;
+    var mgr = new SchemaManager(Config.migration.schema, Config.db);
+    var op = _.get(mgr, operation);
+    return op.bind(mgr).apply(null, arguments)
+        .then(callback)
+        .catch(function(e) {
+            console.error(e);
+            exitCode = 1;
+        })
+        .finally(function() {
+            mgr.close();
+            process.exit(exitCode)
+        })
+}
+
+// `version`
+program
+    .version(pkginfo.version);
+
+// `info` command
+program
+    .command('info')
+    .description('show revision information')
+    .action(function() {
+        return callOperationByName('revision', [], function(revision) {
+            var version = _.get(revision, 'version');
+            console.log("Schema Version:", version);
+            if (version.toLowerCase() != "unknown") {
+                var table = new Table({
+                    head: ['Script', 'Description', 'Execution Time', 'Status']
+                });
+                _.each(revision.migrations || [], function(migration) {
+                    table.push(
+                        [
+                            _.get(migration, "script", "N/A"),
+                            _.get(migration, "description", "N/A"),
+                            _.get(migration, "execution_time", "0") + " ms",
+                            _.get(migration, "status", "N/A")
+                        ]
+                    );
+                });
+                _.each(table.toString().split("\n"), function(line) {
+                    console.log(line);
+                });
+            }
+        });
+    });
+
+// `clean`
+program
+    .command('clean')
+    .description('drops all objects in the managed schema')
+    .action(function() {
+        return callOperationByName('clean', []);
+    });
+
+// `baseline`
+program
+    .command('baseline <version>')
+    .description("baseline existing schema to initial version")
+    .action(function(version) {
+        return callOperationByName('baseline', [version]);
+    });
+
+// `migrate`
+program
+    .command('migrate [version]')
+    .description("migrate schema to new version")
+    .action(function(version) {
+        return callOperationByName('migrate', [Config.migration.datadir, version]);
+    });
+
+// parse command line arguments
+program.parse(process.argv);
+
+// prompt exit code on exit
+process.on('exit', function(code) {
+    console.log('Exit with status code %s', code);
+});
